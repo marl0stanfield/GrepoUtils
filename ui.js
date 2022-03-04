860e4b8a1547ac7addeced0fa0c858dfbc685585
marl0stanfield <96742086+marl0stanfield@users.noreply.github.com>
2022-03-04 02:23:37 +0100

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,60 +1,60 @@
 GM_config.init(
 {
-  'id' : 'MaConfig',
+  'id': 'MyConfig',
   'title': '[Vib-R] GrepoUtils v1.1.2',
-  'fields' : // objet champs
+  'fields': // Fields object
   {
-    'TozsdeBot_Enabled' :
+    'TozsdeBot_Enabled':
     {
-      'étiquette' : 'Activé',
-      'section' : ['ExchangeBot'],
+      'label': 'Activé',
+      'section': ['TőzsdeBot'],
       'title': 'Si le débit de stock doit être activé.',
-      'type' : 'case à cocher',
-      'par défaut' : faux
+      'type': 'checkbox',
+      'default': false
     },
-     'TozsdeBot_TickInterval' :
+     'TozsdeBot_TickInterval':
     {
-      'étiquette' : 'Fréquence',
-      'type' : 'entier',
+      'label': 'Fréquence',
+      'type': 'int',
       'title': 'Combien de secondes pour passer à la prochaine ville.',
-      'par défaut' : '5'
+      'default': '5'
     },
     'TozsdeBot_MinimalResource':
     {
-      'label' : 'Quantité minimale de matière première',
-      'type' : 'entier',
+      'label': 'Quantité minimale de matière première',
+      'type': 'int',
       'title': 'Quel devrait être le produit minimum que vous envoyez à la bourse.',
-      'par défaut' : '150'
+      'default': '150'
     },
-    'TozsdeBot_CaptchaDetection' :
+    'TozsdeBot_CaptchaDetection':
     {
-      'label' : 'Captcha détection',
+      'label': 'Captcha détection',
       'title': 'Reconnaît quand le captcha est affiché.',
-      'type' : 'case à cocher',
-      'par défaut' : vrai
+      'type': 'checkbox',
+      'default': true
     },
     'TozsdeBot_URL':
     {
       'label': 'Captcha Hang URL',
-      'type' : 'texte',
+      'type': 'text',
       'title': 'Si le captcha est affiché, jouez ce son.',
-      'taille' : 250,
-      'par défaut' : 'https://notificationsounds.com/storage/sounds/file-sounds-1134-open-up.mp3'
+      'size': 250,
+      'default': 'https://notificationsounds.com/storage/sounds/file-sounds-1134-open-up.mp3'
     },
-    'AntiGrepolisUpdate_Enabled' :
+    'AntiGrepolisUpdate_Enabled':
     {
-      'étiquette' : 'Activé',
+      'label': 'Bekapcsolva',
       'section': ['Actualiser la page après mise à jour des serveurs grepolis'],
       'title': 'Pour actualiser la page. (Regarde chaque minute) ',
-      'type' : 'case à cocher',
-      'par défaut' : faux
+      'type': 'checkbox',
+      'default': false
     },
-    'Villes':
+    'Cities':
     {
-      'label' : 'Városok', // Apparaît à côté du champ
-      'type' : 'select', // Fait de ce paramètre une liste déroulante
-      'options' : ['-'], // Choix possibles
-      'default' : '-' // Valeur par défaut si l'utilisateur ne la modifie pas
+      'label': 'Városok', // Appears next to field
+      'type': 'select', // Makes this setting a dropdown
+      'options': ['-'], // Possible choices
+      'default': '-' // Default value if user doesn't change it
     }
   }
 });
